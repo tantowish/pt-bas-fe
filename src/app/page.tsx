52e16d6183b43195d22f03002f603cd81be47994
tanto <tantows001@gmail.com>
2024-01-17 08:56:14 +0700

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,127 @@
+'use client'
+
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+interface OngoingItem {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
 
 export default function Home() {
+  const [onGoingData, setOngoingData] = useState<OngoingItem[]>([])
+
+  useEffect(()=>{
+    const fetchData =async () => {
+      try{
+        const response = await fetch('/data/ongoing.json')
+        const data: OngoingItem[] = await response.json();
+        setOngoingData(data)
+
+      } catch(error){
+        console.error('Error fetching data: ', error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className="pt-8">
+          <div className="max-w-7xl px-8 lg:px-32 lg:py-24 mx-auto">
+              <div className="flex flex-wrap h-full items-center justify-center">
+                  <div className="lg:w-1/2 w-full lg:pr-4 pr-0 py-16">
+                      <h4 className="text-slate-400 mb-2 anima">00 - Hero Section</h4>
+                      <h1 className="text-slate-900 text-3xl lg:text-5xl mb-5 lg:mb-8">Lorem Ipsum Dolor sit Amet</h1>
+                      <p className="text-slate-400 leading-loose mb-4 text-sm lg:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing  
+                          elit, sed do eiusmod tempor incididunt ut
+                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                          ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      </p>
+                      <div className="flex flex-wrap gap-4">
+                          <button type="button" className="lg:text-base text-sm py-2 px-5  text-white  bg-[#0F1957] hover:bg-[#29357E] transition ease-in-out duration-500 rounded-full">
+                              EXPLORE MORE +
+                          </button>
+                          <button type="button" className="lg:text-base text-sm py-2 px-5 text-white bg-[#0F1957] hover:bg-[#29357E] transition ease-in-out duration-500 rounded-full">
+                              BUTTON 2
+                          </button>
+                      </div>
+                  </div>
+                  
+                  <div className="lg:w-1/2 max-w-xl lg:pl-4 pr-0 group relative py-8 flex justify-center">
+                      <div>
+                          <Image 
+                          src="/home/hero.png" 
+                          alt=""
+                          width={400}
+                          height={400}
+                          />
+                      </div>
+                      <div className="absolute lg:left-16 lg:top-20 top-16 left-8">
+                          <div data-aos="zoom-in" data-aos-delay="600" data-aos-duration="500" className="border p-3 border-black rounded-full">
+                              <svg className="group-hover:rotate-90 transition ease-in-out duration-700" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1024 1024"><path fill="currentColor" d="M768 256H353.6a32 32 0 1 1 0-64H800a32 32 0 0 1 32 32v448a32 32 0 0 1-64 0z"/><path fill="currentColor" d="M777.344 201.344a32 32 0 0 1 45.312 45.312l-544 544a32 32 0 0 1-45.312-45.312z"/></svg>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+
+      <section>
+          <div className="max-w-5xl px-8 py-16 lg:px-32 lg:pt-24 lg:pb-32 mx-auto text-center">
+              <h4 className="text-slate-400 mb-2">01 - About Us</h4>
+              <h1 className="text-slate-900 text-3xl lg:text-5xl mb-5 lg:mb-8">Tentang Kami</h1>
+              <p className="text-slate-400 leading-loose text-sm lg:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+                  do eiusmod tempor incididunt ut
+                  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+          </div>
+          <hr data-aos="zoom-in" className="max-w-xs mx-auto p-1 bg-[#FFB13C]"/>
+      </section>
+
+      <section>
+        <div className="max-w-7xl px-8 py-16 lg:px-32 lg:pb-24 lg:pt-32 mx-auto">
+            <div className="text-center">
+                <h4 className="text-slate-400 mb-2">03 - Ongoing Project</h4>
+                <h1 className="text-slate-900 text-3xl lg:text-5xl mb-12">Proyek Berlangsung</h1>
+            </div>
+            <div className='w-full flex justify-center max-w-2xl mx-auto'>
+              <iframe className='aspect-video w-full' src="https://www.youtube.com/embed/TJooF1REhFU?si=dWVTYKaJeNRtLX3q" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div>
+            <hr className="mx-auto border-[1.5px] my-8 lg:my-16 bg-slate-800" />
+            <div className="grid lg:grid-cols-2 grid-cols-1  gap-8 mb-8 lg:mb-12">
+              <ul>
+                {onGoingData.map(item => (
+                  <li key={item.id}>
+                    <p>ID: {item.id}</p>
+                    <p>Image: {item.img}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='flex flex-wrap justify-center'>
+              <button type="button" className="lg:text-base text-sm py-2 px-5  text-white  bg-[#0F1957] hover:bg-[#29357E] transition ease-in-out duration-500 rounded-full">
+              See all project
+              </button>
+            </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section>
+        <div className="text-center mt-24">
+            <h4 className="text-slate-400 mb-2">03 - Our Location</h4>
+            <h1 className="text-slate-900 text-3xl lg:text-5xl mb-12">Lokasi Kami</h1>
+        </div>
+        <hr className="mx-8 lg:mx-32"/>
+        <iframe 
+        style={{ border:"0" }}
+        className="w-full h-44 lg:h-80" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.3835885535013!2d104.74973657472968!3d-2.9908830969851574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b75fdffef3991%3A0x8eb938d31f7279f7!2sPT.%20BAS!5e0!3m2!1sen!2sid!4v1704285179558!5m2!1sen!2sid" width="600" height="450"   loading="lazy"></iframe>
+      </section>
     </main>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="flex flex-wrap p-8 lg:px-32 lg:py-16 mx-auto items-center">
+            <div className="w-full md:w-1/2 flex flex-wrap">
+                <div className="max-w-md">
+                    <h3 className="text-sm font-semibold lg:text-base mb-2 lg:mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore.</h3>
+                    <p className="text-xs lg:text-sm">Jl. Merdeka No.3 E, Talang Semut, Kec. Bukit Kecil,
+                        Kota Palembang, Sumatera Selatan 30135</p>
+                </div>
+            </div>
+            <div className="w-full md:w-1/2 text-sm  md:text-bas md:flex md:flex-row-reverse py-4 md:mt-0">
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-wrap gap-2 items-center">
+                        <a href="" className="underline">INSTAGRAM</a>
+                        <svg className="rotate-45" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 36 36"><path fill="currentColor" d="M27.66 15.61L18 6l-9.66 9.61A1 1 0 1 0 9.75 17L17 9.81v19.13a1 1 0 1 0 2 0V9.81L26.25 17a1 1 0 0 0 1.41-1.42Z" className="clr-i-outline clr-i-outline-path-1"/><path fill="none" d="M0 0h36v36H0z"/></svg>
+                    </div>
+                    <div className="flex flex-wrap gap-2 items-center">
+                        <a href="" className="underline">FACEBOOK</a>
+                        <svg className="rotate-45" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 36 36"><path fill="currentColor" d="M27.66 15.61L18 6l-9.66 9.61A1 1 0 1 0 9.75 17L17 9.81v19.13a1 1 0 1 0 2 0V9.81L26.25 17a1 1 0 0 0 1.41-1.42Z" className="clr-i-outline clr-i-outline-path-1"/><path fill="none" d="M0 0h36v36H0z"/></svg>
+                    </div>
+                    <div className="flex flex-wrap gap-2 items-center">
+                        <a href="" className="underline">E-MAIL</a>
+                        
+                        <svg className="rotate-45" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 36 36"><path fill="currentColor" d="M27.66 15.61L18 6l-9.66 9.61A1 1 0 1 0 9.75 17L17 9.81v19.13a1 1 0 1 0 2 0V9.81L26.25 17a1 1 0 0 0 1.41-1.42Z" className="clr-i-outline clr-i-outline-path-1"/><path fill="none" d="M0 0h36v36H0z"/></svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
