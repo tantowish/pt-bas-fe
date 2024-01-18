918117a8878b1e5de36156ece6a6f1f87711f038
tanto <tantows001@gmail.com>
2024-01-18 16:17:28 +0700

    fix: padding top

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,8 +10,8 @@ interface portfolio {
 
 const Projects = () => {
   return (
-    <div className='pt-10 lg:pt-8'>
-      <div className="max-w-7xl px-8 lg:px-24 lg:py-24 md:px-16 md:py-16 mx-auto">
+    <div className='pt-16 lg:pt-8'>
+      <div className="max-w-7xl p-8 lg:px-24 lg:py-24 md:px-16 md:py-16 mx-auto">
         <div className="text-start">
           <h4 className="text-slate-600 mb-2">PT BAS - Projects</h4>
           <h1 className="text-slate-900 text-3xl lg:text-5xl mb-12 font-semibold">Alamanda Bachiro</h1>
