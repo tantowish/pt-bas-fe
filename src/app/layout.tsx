d0aa7182f28fc6ff566eb107adcfafba3fc56fe2
tanto <tantows001@gmail.com>
2024-02-27 13:39:55 +0700

    feat: SEO Optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,14 @@ import './globals.css'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 
-const poppins = Poppins({ 
-  subsets: ['latin'] ,
-  weight: ['300','400', '500', '600', '700']
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
 })
 
 export const metadata: Metadata = {
   title: 'PT Bintang Andalas Selatan',
-  description: 'Menyediakan berbagai layanan konstruksi, mulai dari pembangunan rumah tinggal, gedung, hingga infrastruktur. Dengan tim yang berpengalaman dan berdedikasi, kami siap mewujudkan impian Anda untuk memiliki hunian atau bangunan yang nyaman dan berkualitas.',
+  description: 'PT Bintang Andalas, Alamat Jl. Merdeka No.3 E, Talang Semut, Kec. Bukit Kecil, Kota Palembang, Sumatera Selatan 30135. Menyediakan berbagai layanan konstruksi, mulai dari pembangunan rumah tinggal, gedung, hingga infrastruktur.',
 }
 
 export default function RootLayout({
