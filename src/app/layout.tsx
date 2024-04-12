abd2f2db073dfd0fe23b6c91d7efd13f450ce741
tanto <tantows001@gmail.com>
2024-04-12 07:33:43 +0700

    feat: speed insight vercell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,6 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
+      <SpeedInsights/>
       <body className={poppins.className}>
         <Navbar />
         {children}
